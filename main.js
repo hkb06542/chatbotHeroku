51bacab7afcd117f7f2595737c7412c9ab456f47
Barapatre, Hemant <hbarapat@akamai.com>
2018-04-11 13:05:40 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+//Developer @ Hemant Barapatre
+var Botkit = require('Botkit');
+
+var controller = Botkit.sparkbot({
+    debug: true,
+    log: true,
+    public_address: 'localhost:8080',
+    ciscospark_access_token: 'ZmIyNjk0YjEtNzk2Yy00MzI5LTkxYzYtNjU4MGE1ZWZkOGQ1NmJiMWExZjMtNzBl',
+    secret: 'wowwowclear'
+});
+
+
+var bot = controller.spawn({
+});
+
+var webserver = controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        console.log("SPARK: Webhooks set up!");
+    });
+});
+
+controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
+    bot.reply(message, 'Hi');
+});
+
+controller.on('direct_mention', function(bot, message) {
+    bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+});
+
+controller.on('direct_message', function(bot, message) {
+    bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+});
+
+console.log(webserver);
